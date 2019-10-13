6789757b1aa7bd779ef0567c386202b335517172
DankoMaksym <dankomaksym@gmail.com>
2019-10-13 17:39:22 +0300

    menu transition fixed

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+document.querySelector('#toggle-button').addEventListener('click', function() {
+  document.querySelector('.section.collapsible').classList.toggle('collapsed');
+});
