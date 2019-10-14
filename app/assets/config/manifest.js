4bc62270a1491ded62ea21ad6000a8876a968d8d
DankoMaksym <dankomaksym@gmail.com>
2019-10-15 01:07:57 +0300

    Side menu fixed

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,16 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 document.querySelector('#toggle-button').addEventListener('click', function() {
